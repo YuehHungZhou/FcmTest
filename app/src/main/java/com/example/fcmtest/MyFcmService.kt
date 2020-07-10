0b020a2f7a4c731fdaec69f1a7774267d9ec06e9
Dante <yuehhung_zhou@17life.com>
2020-07-10 12:39:42 +0800

    show notification

--- app/src/main/java/com/example/fcmtest/MyFcmService.kt
+++ app/src/main/java/com/example/fcmtest/MyFcmService.kt
@@ -1,12 +1,17 @@
 package com.example.fcmtest
 
 import android.R
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.media.AudioAttributes
 import android.media.RingtoneManager
 import android.net.Uri
+import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
@@ -16,17 +21,15 @@ import com.google.firebase.messaging.RemoteMessage
 class MyFcmService : FirebaseMessagingService() {
     companion object {
         const val TAG: String = "MyFcmService"
+
+        const val test_chennel_id = "test_channel_id"
     }
 
     override fun onNewToken(token: String) {
         Log.d(TAG, "Refreshed token: " + token)
-//        sendRegistrationToServer(token)
     }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
 
         Log.d("MyFcmService", "~~~~~~~~~~~~~ start ~~~~~~~~~~~~~")
 
@@ -62,7 +65,6 @@ class MyFcmService : FirebaseMessagingService() {
         }
 
         val intent = Intent(this, MainActivity::class.java)
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
 
         val pendingIntent = PendingIntent.getActivity(
             this, 0 /* Request code */, intent,
@@ -70,17 +72,55 @@ class MyFcmService : FirebaseMessagingService() {
         )
 
         val defaultSoundUri: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val mNotificationManager =
+            applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val mChannel: NotificationChannel
 
-        val notificationBuilder = NotificationCompat.Builder(this)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            mChannel = NotificationChannel(
+                test_chennel_id,
+                "test_channel_name",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+
+            mChannel.lightColor = Color.GRAY
+
+            mChannel.enableLights(true)
+
+            mChannel.description = "test_channel_description"
+
+            val audioAttributes = AudioAttributes.Builder()
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+                .build()
+            mChannel.setSound(defaultSoundUri, audioAttributes)
+
+            mNotificationManager.createNotificationChannel(mChannel)
+        }
+
+
+        val notificationBuilder = NotificationCompat.Builder(this, test_chennel_id)
             .setSmallIcon(R.mipmap.sym_def_app_icon)
+            .setLargeIcon(
+                BitmapFactory.decodeResource(
+                    applicationContext.resources,
+                    R.mipmap.sym_def_app_icon
+                )
+            )
+            .setTicker("FCM Ticker")
             .setContentTitle("FCM Message")
             .setContentText(messageBody)
             .setAutoCancel(true)
-            .setSound(defaultSoundUri)
+            .setLights(0xff0000ff.toInt(), 300, 1000)
+            .setWhen(System.currentTimeMillis())
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             .setContentIntent(pendingIntent)
 
-        val notificationManager =
-            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            notificationBuilder.setSound(defaultSoundUri);
+        }
+
+        mNotificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
     }
 }
